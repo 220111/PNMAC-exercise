b57a6f604b749b888ccf2a5fc5e8edee4412f32a
Henry Morin <morinh@rose-hulman.edu>
2026-06-09 12:51:55 -0400

    Backend: added base stack

---
+++ packages/backend/lib/base-stack.ts
@@ -0,0 +1,34 @@
+import * as cdk from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as logs from 'aws-cdk-lib/aws-logs';
+
+export class BaseStack extends cdk.Stack {
+    public readonly ingestTable: dynamodb.Table;
+    public readonly apiSecret: secretsmanager.Secret;
+    public readonly sharedLogGroup: logs.LogGroup;
+
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        this.ingestTable = new dynamodb.Table(this, 'IngestTable', {
+            partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
+            sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING },
+        });
+
+        this.apiSecret = new secretsmanager.Secret(this, 'ExternalApiSecret', {
+            secretName: 'ingest/massive-api-key',
+            description: 'API key for stock data ingest from Massive',
+        });
+
+        this.sharedLogGroup = new logs.LogGroup(this, 'SharedLambdaLogs', {
+            logGroupName: '/aws/lambda/pnmac-backend',
+            retention: logs.RetentionDays.TWO_WEEKS,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+        });
+
+        new cdk.CfnOutput(this, 'DynamoDbTableName', { value: this.ingestTable.tableName });
+        new cdk.CfnOutput(this, 'SecretArn', { value: this.apiSecret.secretArn });
+    }
+}
